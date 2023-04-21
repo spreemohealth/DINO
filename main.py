9d6e9f70afce61b24911efccd237afb04e9bc6a8
ashstuff <ashwin.raju@coverahealth.com>
2023-04-21 13:21:41 -0700

    added

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def get_args_parser():
     parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
                         help='start epoch')
     parser.add_argument('--eval', action='store_true')
-    parser.add_argument('--num_workers', default=s0, type=int)
+    parser.add_argument('--num_workers', default=0, type=int)
     parser.add_argument('--test', action='store_true')
     parser.add_argument('--debug', action='store_true')
     parser.add_argument('--find_unused_params', action='store_true')
